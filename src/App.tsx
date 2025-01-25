e62e40be65e61da91a280b2195a5bbc944dc24dd
reaperxyndrome <crispy_crab_777@protonmail.com>
2025-01-25 14:16:34 +1100

    design is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import EthereumIcon from "./assets/images/icon-ethereum.svg";
+import ClockIcon from "./assets/images/icon-clock.svg";
+import ViewIcon from "./assets/images/icon-view.svg";
+import EquilibriumImage from "./assets/images/image-equilibrium.jpg";
+import AvatarImage from "./assets/images/image-avatar.png";
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-lg">App</h1>
-    </>
+    <div className="bg-secondary-main-BG h-[100vh]">
+      <section className="absolute font-outfit w-card h-card bg-secondary-card-BG p-6 pb-8 max-md:pb-6 rounded-2xl top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 max-md:w-[327px] max-md:h-fit">
+        <div className="relative mb-6">
+          <img src={EquilibriumImage} className="rounded-lg" />
+          <div className="absolute flex justify-center items-center inset-0 bg-primary-cyan/50 opacity-0 hover:opacity-100 hover:cursor-pointer rounded-lg">
+            <img src={ViewIcon}></img>
+          </div>
+        </div>
+        <h1 className="text-heading font-semibold text-[white] mb-4 max-md:mb-3 max-md:leading-7 hover:text-primary-cyan hover:cursor-pointer">  Equilibrium #3429</h1>
+        <p className="text-body text-primary-soft-blue font-light mb-6 max-md:mb-4">  Our Equilibrium collection promotes balance and calm.</p>
+        <div className="flex justify-between items-center mb-6 max-md:mb-4">
+          <div className="flex">
+            <img src={EthereumIcon} className="inline self-center mr-1.5" />
+            <span className="text-caption text-primary-cyan"> 0.041 ETH </span>
+          </div>
+          <div className="flex">
+            <img src={ClockIcon} className="inline self-center mr-2" />
+            <span className="text-caption text-primary-soft-blue"> 3 days left</span>
+          </div>
+        </div>
+        <div className="h-[1px] bg-secondary-line mb-4"></div>
+        <img src={AvatarImage} className="inline w-8 h-8 mr-4 border border-white rounded-full " />
+        <p className="inline w-fit text-primary-soft-blue">Creation of <span className="text-[white] hover:text-primary-cyan hover:cursor-pointer">Jules Wyvern</span></p>
+      </section>
+
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-soft-blue': 'hsl(215, 51%, 70%)',
+        'primary-cyan': 'hsl(178, 100%, 50%)',
+        'secondary-main-BG': 'hsl(217, 54%, 11%)',
+        'secondary-card-BG': 'hsl(216, 50%, 16%)',
+        'secondary-line': 'hsl(215, 32%, 27%)'
+      },
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif']
+      },
+      width: {
+        "card": '21.875rem',
+        "inner_card": '18.875rem',
+        "card_mobile": '20rem',
+      },
+      height: {
+        "card": '37.25rem',
+        "thumbnail": '18.875rem',
+      },
+      fontSize: {
+        heading: '1.375rem',
+        body: '1.125rem',
+        caption: '1rem',
+        caption_mobile: '0.9375rem'
+      }
+    },
   },
   plugins: [],
 }
